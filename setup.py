1a394fcf8c980d0b81d08520e1b73701b65f2c50
VAN HAAREN Lucas <vanhaarenlucas.dev@gmail.com>
2020-11-21 22:36:15 +0100

    add package data in sdist

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ from setuptools import setup, find_packages
 setup(
     name='orbelican',
     version='0.1.0',
-    packages=find_packages(),
     sass_manifests={
         '.': {
             'sass_path': 'sass/themes',
@@ -12,6 +11,7 @@ setup(
             'strip_extension': True,
         },
     },
+    include_package_data=True,
     setup_requires=['libsass >= 0.20.0'],
     # project metadata
     description='A simple port of orbit-theme for Pelican to build your online resume',
